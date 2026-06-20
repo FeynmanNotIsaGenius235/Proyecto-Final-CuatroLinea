470d425cf6ee0cf650b2e607a4612a44d2d03129
Federico Gonzalez <fegonzalezc@unal.edu.co>
2026-06-20 15:26:04 -0500

    Funciones Colocar Ficha,y prototipo Verificar el ganador

--- CuatroLinea.cpp
+++ CuatroLinea.cpp
@@ -29,7 +29,7 @@ void MostrarTablero(char Tablero[6][7]){
 	}
 }
 
-int FilaDisponible(char Tablero[6][7],int ColumnaSeleccionada,int FilaActual=5){
+int FilaDisponible(char Tablero[6][7],int ColumnaSeleccionada,int FilaActual=6){
 	
 	if(FilaActual<0){
 		return -1;
@@ -38,45 +38,114 @@ int FilaDisponible(char Tablero[6][7],int ColumnaSeleccionada,int FilaActual=5){
 	if(Tablero[FilaActual][ColumnaSeleccionada]=='.'){
 		return FilaActual;
 	}
-	
+		
 	return FilaDisponible(Tablero,ColumnaSeleccionada,FilaActual-1);
 }
 
-bool jugadaValida(char Tablero[6][7],int ColumnaSeleccionada){
+int ColocarFicha(char Tablero[6][7],int columna, char jugador){
 	
-}
-
-
-void ColocarFicha(char Tablero[6][7],int columna, char jugador){
+	int Fila=FilaDisponible(Tablero,columna);
 	
-	int fila=FilaDisponible(Tablero,columna);
-	
-	if(columna<7 && fila!=-1){
-		
-		Tablero[fila][columna-1]=jugador;
-		
+	if(Fila!=-1 && (columna>=0 && columna<=6)){
+		Tablero[Fila][columna]=jugador;
+		return Fila;
 	}else{
-		cout<<"\n\n"<<"Jugada Inválida.Columna Fuera de Rango o Columna Llena"<<"\n\n";
+
 	}
-				
+	
 }
-int main() {
+
+int ContarFichas(char Tablero[6][7], int fila, int columna, char Jugador){
+	
+	if(fila<0){
+		return 0;
+	}
+	
+	if(Tablero[fila][columna]!=Jugador){
+		return 0;
+	}
+		
+	return 1+ContarFichas(Tablero,fila-1,columna,Jugador);
+}
+
+
+bool verificarGanador(char Tablero[6][7], int fila, int columna, char Jugador){
+	
+	int FichasSeguidas=ContarFichas(Tablero,fila,columna,Jugador);
+	
+	if(FichasSeguidas>=4){
+		return true;
+	}else{
+		return false;
+	}
+	
+}
+
+
+int main(){
+	
 	char TableroJuego[6][7];
 	
 	InicializarTablero(TableroJuego);
 	
 	MostrarTablero(TableroJuego);
 	
-	ColocarFicha(TableroJuego,3,'#');
+	int Fila=ColocarFicha(TableroJuego,0,'#');
 	
 	MostrarTablero(TableroJuego);
 	
-	ColocarFicha(TableroJuego,7,'#');
+	cout<<Fila<<endl;
+	
+	Fila=ColocarFicha(TableroJuego,0,'#');
 	
 	MostrarTablero(TableroJuego);
-
-
-
+	
+	cout<<Fila<<endl;
+	
+	Fila=ColocarFicha(TableroJuego,0,'#');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila<<endl;
+	
+	cout<<verificarGanador(TableroJuego,5,0,'#')<<endl;
+	
+	Fila=ColocarFicha(TableroJuego,0,'#');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila<<endl;
+	
+	cout<<verificarGanador(TableroJuego,5,0,'#');
+	
+	int Fila1=ColocarFicha(TableroJuego,1,'$');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila1<<endl;
+	
+	Fila1=ColocarFicha(TableroJuego,1,'$');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila1<<endl;
+	
+	Fila1=ColocarFicha(TableroJuego,1,'$');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila1<<endl;
+	
+	cout<<verificarGanador(TableroJuego,5,1,'$')<<endl;
+	
+	Fila1=ColocarFicha(TableroJuego,1,'$');
+	
+	MostrarTablero(TableroJuego);
+	
+	cout<<Fila1<<endl;
+	
+	cout<<verificarGanador(TableroJuego,5,1,'$');
+		
     return 0;
 }
 
